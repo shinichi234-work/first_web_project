51d7d1c145cc7eadd6efae10ef814d895a2d8cda
shinichi234-work <1334shinichi@gmail.com>
2026-05-12 19:51:00 +0300

    Task 5 ready.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+    views_count = models.IntegerField(default=0, verbose_name='Views')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Published')
+
+    def __str__(self):
+        return self.title
