cf8ed55985754247d1cb5fa2092fc587f3874b9b
shinichi234-work <1334shinichi@gmail.com>
2026-05-14 20:38:00 +0300

    Task 6 ready.

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 
 def index(request):
+    articles = Article.objects.all()
     context = {
         'title': 'RaceHub',
         'welcome_text': 'Your Ultimate Racing News Hub',
+        'articles': articles,
     }
     return render(request, 'pages/index.html', context)
 
